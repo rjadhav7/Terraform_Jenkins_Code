3b337fac70b3e21017c7825902c7b57d4fbbd0e2
Rangnath Jadhav <88224106+rjadhav7@users.noreply.github.com>
2022-01-02 21:50:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,10 +12,9 @@ installJenkins()
 {
 echo “Dependencies installed now Jenkins will be installed”
 
-sudo wget –O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 sudo yum install epel-release java-11-openjdk-devel -y
 
